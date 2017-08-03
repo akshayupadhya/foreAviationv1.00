be0dea8d1bb2a20f1a722907e68ad6e6aad592a9
akshay upadhya <akshaysrinagar@gmail.com>
2017-08-03 16:57:36 +0530

    navigation-drawer#bugfix

--- components/navigation.vue
+++ components/navigation.vue
@@ -1,15 +1,15 @@
 <template lang="pug">
 .nav
-    v-navigation-drawer(v-model="drawer" )
+    v-navigation-drawer(v-model="drawer" persistent=true)
         v-toolbar( flat class="transparent")
             v-list( class="pa-0")
                 router-link(to="/") 
                     v-list-tile( tag="ul") fA
         v-divider
         v-list( dense class="pt-0")
-            v-list-tile( v-for="(item,$index) in list" :key="$index")
-                nuxt-link( :to="item.link") 
-                    v-list-tile-title( dark  ) {{ item.title}}
+                v-list-tile( v-for="(item,$index) in list" ripple :key="$index")
+                   nuxt-link(  active-class="navbar__item--active",v-bind:exact="item.link === '/'",  v-bind:to="item.link") 
+                            v-list-tile-title {{ item.title}}
     v-toolbar( dark )
         v-toolbar-side-icon(@click.native.stop="drawer = !drawer")
         v-toolbar-title(class="white--text") 
