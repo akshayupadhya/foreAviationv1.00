2a59beb5f0da7d339128f4ede38b68e106f736a9
Akshay Upadhya <akshaysrinagar@gmail.com>
2017-08-15 08:53:33 +0530

    home-navigation#styling

--- components/navlist-home.vue
+++ components/navlist-home.vue
@@ -1,19 +1,20 @@
 <template lang="pug">
   .linklist
     ul
-        li(v-for="item in $store.state.pageComponents.list" ) 
+        li(v-for="(item,i) in $store.state.pageComponents.list" v-if="i!=0") 
             router-link(:to="item.link") 
                  v-btn(dark) {{item.title}}
 </template>
 <style lang="scss" scoped>
 .linklist{
     //border: 1px solid whitesmoke;
-    width: 100%;
+    width: 120%;
 
 }
     ul{
         display: flex;
-        justify-content: space-between;
+        flex-wrap: wrap;
+        justify-content: space-around;
         a{
             text-decoration: none;
         }
